e77cfcbebcbc6caad066eb852d9fb8a8fdaf08f3
Chiragpatel1189 <chiragrpatel812@gmail.com>
2022-11-30 23:01:16 +0530

    highlight possible steps for a knight & format code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import Parser from "html-react-parser";
 import "./App.css";
+import { useEffect, useState } from "react";
+import Parser from "html-react-parser";
 
 const App = () => {
     let whiteknight = "&#9816";
@@ -94,12 +94,26 @@ const App = () => {
     const [selected, setSelected] = useState({});
     const [row, setRow] = useState();
     const [col, setCol] = useState();
+    const [possibleLocation, setPossibleLocation] = useState([]);
+    const [filterLocation, setFilterLocation] = useState([]);
 
+    useEffect(() => {
+        setFilterLocation(
+            possibleLocation.reduce((acc, item) => {
+                if (item.i >= 0 && item.i < 8 && item.j >= 0 && item.j < 8) {
+                    return [...acc, item];
+                } else return [...acc];
+            }, []),
+        );
+    }, [possibleLocation]);
+
+    console.log(filterLocation, "|filterLocation");
     const swapFn = (i, j, row, col) => {
         temp[i][j].img = temp[row][col].img;
         temp[row][col].img = "";
         setChessData([...temp]);
         setSelected({});
+        setFilterLocation([]);
     };
 
     const getID = (i, j) => {
@@ -107,6 +121,7 @@ const App = () => {
             setSelected(temp[i][j]);
             setRow(i);
             setCol(j);
+            getPossiblePath(i, j);
         }
         if (selected.img) {
             getSecondClickid(i, j);
@@ -121,12 +136,27 @@ const App = () => {
             swapFn(i, j, row, col);
         }
     };
+    const getPossiblePath = (i, j) => {
+        console.log(i, "i", j, "j");
+        setPossibleLocation([
+            { i: i + 2, j: j + 1 },
+            { i: i + 2, j: j - 1 },
+            { i: i - 2, j: j + 1 },
+            { i: i - 2, j: j - 1 },
+            { i: i + 1, j: j + 2 },
+            { i: i - 1, j: j + 2 },
+            { i: i + 1, j: j - 2 },
+            { i: i - 1, j: j - 2 },
+        ]);
+    };
 
     const getSecondClickid = (i, j) => {
         if ((temp[row][col].img === whiteknight || temp[row][col].img === blackknight) && temp[i][j].img === "") {
             knightPath(i, j, row, col);
         }
     };
+
+    console.log("selected", selected);
 
     return (
         <div className="App">
@@ -139,17 +169,20 @@ const App = () => {
                                 return (
                                     <div
                                         key={j}
-                                        className={
-                                            (i + 1) % 2 === 0
-                                                ? item.col % 2 === 0
-                                                    ? `greenCol chessboxDiv ${!item.img ? "hover" : ""}`
-                                                    : "whiteCol chessboxDiv"
-                                                : item.col % 2 !== 0
-                                                ? "greenCol chessboxDiv"
-                                                : "whiteCol chessboxDiv"
-                                        }
+                                        className={`
+                      ${filterLocation.find(item => item.i === i && item.j === j) && "darkGreen"}
+                      ${
+                          (i + 1) % 2 === 0
+                              ? item.col % 2 === 0
+                                  ? `greenCol chessboxDiv ${!item.img ? "hover" : ""}`
+                                  : "whiteCol chessboxDiv"
+                              : item.col % 2 !== 0
+                              ? "greenCol chessboxDiv"
+                              : "whiteCol chessboxDiv"
+                      }
+                      `}
                                         onClick={() => getID(i, j)}>
-                                        <p className={selected === item ? "borderclass htmlunicode" : "htmlunicode"}>{Parser(item.img)} </p>
+                                        <p className={selected === item ? "borderclass htmlunicode" : "htmlunicode"}>{Parser(item.img)}</p>
                                     </div>
                                 );
                             })}
@@ -158,8 +191,10 @@ const App = () => {
                 })}
             </div>
             <div className="colNamediv">
-                {colName.map(list => (
-                    <p className="chessboxDiv">{list}</p>
+                {colName.map((list, index) => (
+                    <p className="chessboxDiv" key={index}>
+                        {list}
+                    </p>
                 ))}
             </div>
         </div>
